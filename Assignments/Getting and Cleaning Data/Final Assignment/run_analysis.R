9cf77c13fea5ee2fc53259d0faa1353c0854ca60
KimberlyBorm <kabormann@gmail.com>
2025-02-27 22:13:47 -0500

    Completed Steps 4, label dataset with discriptive names and Step 5, create tidy dataset with the average of each variable for each activity and subject. Wrote CSV file on final tidy dataset

--- Assignments/Getting and Cleaning Data/Final Assignment/run_analysis.R
+++ Assignments/Getting and Cleaning Data/Final Assignment/run_analysis.R
@@ -15,7 +15,7 @@ if (!file.exists(destfile)) {
 }
 #unzip folder and name
 unzip(destfile, exdir = "Final Assignment Data")
-#Function to read data baseed on filestructure and READMe for test and train files
+#Function to read data baseed on filestructure and READMe for test and train files since it needs to be completed more than once
 read_data <- function(type){
     #folder path in base directory to merge multiple files
     folder_path <- file.path("Final Assignment Data", "UCI HAR Dataset", type)
@@ -52,10 +52,40 @@ readr::write_csv(full_data, "combined_dataset.csv")
 #check data 
 head(full_data)
 
+
 # Task 2) Extracts only the measurements on the mean and standard deviation for each measurement. 
+#read feature file and create 2 columns for later use
+features <-readr::read_table(file.path("Final Assignment Data", "UCI HAR Dataset", "features.txt"),
+                             col_names = c("feature_num", "feature_name"))
+#filter for mean and std in feature names to create T/F (// b/c () are special charter and we want a literal charters))
+features_mean_std <- features %>%
+    dplyr::filter(grepl("mean\\(\\)|std\\(\\)", feature_name))
+#select indices of these features corresponding to X_ data
+selected_indices <- features_mean_std$feature_num
+#adjust for first 3 columns
+measurement_indices <- selected_indices + 3
+#subset full_data
+subset_data <- full_data %>% 
+    dplyr::select(subject, activity, activity_name, dplyr::all_of(measurement_indices))
+#print to check results
+head(subset_data)
+
 
 # Task 3) Uses descriptive activity names to name the activities in the data set
+#completed in Step 1 functions (see above)
+
 
 # Task 4) Appropriately labels the data set with descriptive variable names. 
+colnames(subset_data) [4:ncol(subset_data)] <- features_mean_std$feature_name
+#print to check results
+head(subset_data)
 
-# Task 4) From the data set in step 4, creates a second, independent tidy data set with the average of each variable for each activity and each subject.
+
+# Task 5) From the data set in step 4, creates a second, independent tidy data set with the average of each variable for each activity and each subject.
+avg_data <- subset_data %>%
+    dplyr::group_by(subject, activity, activity_name) %>%
+    dplyr::summarise(across(4:66, mean), .groups = "drop")
+#View new dataset
+head(avg_data)
+#Write average data as csv files (not required but I wanted to)
+write_csv(avg_data, "averagemeanandstd_dataset.csv")
